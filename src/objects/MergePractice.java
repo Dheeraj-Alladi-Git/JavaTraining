6916ccbf29078f257e331f3f26842569b2b1b59b
Dheeraj Alladi <dheeraj.alladi@mail.techsemble.us>
2025-12-16 10:32:41 -0600

    Test Branch change

--- src/objects/MergePractice.java
+++ src/objects/MergePractice.java
@@ -3,5 +3,6 @@ package objects;
 public class MergePractice {
     public static void main(String[] args) {
         System.out.println("Base version on main branch");
+        //Testing branches
     }
 }

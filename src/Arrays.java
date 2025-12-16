5652892f772244825d32e619d265b2bc75aaf4e7
Dheeraj Alladi <dheeraj.alladi@mail.techsemble.us>
2025-12-16 09:54:46 -0600

    Changes in Arrays class

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,14 +1,13 @@
 public class Arrays {
     public static void main(String[] args) {
-
-        /* Problem 1
+        
         int[] arr = {1,2,3,4,5,6,7};
         for(int i=0 ; i<arr.length ; i++){
             if(arr[i]%2==0){
                 System.out.print(arr[i]+" ");
             }
         }
-         */
+
 
 
         /*Problem : 2
